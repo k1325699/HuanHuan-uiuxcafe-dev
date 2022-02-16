940257617496d0c34b2846ee21058cb3ae15ebbd
k1325699 <k1325699@gmail.com>
2022-02-16 16:20:06 +0800

    feat.UIUXcafe測驗

---
+++ Slideshow/main.js
@@ -0,0 +1,6 @@
+let fullPageInstance =new fullpage('#fullpage', {
+  autoScrolling:true,
+	scrollHorizontally: true,
+  sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+  anchors:['Page1', 'Page2', 'Page3','Page4']
+});
